1e10d75271d7ed527c022521ec5f146b227d3af5
Andrew Haarsager <athaarsager@gmail.com>
2023-11-10 15:18:54 -0600

    Create separate row element

--- javascript.js
+++ javascript.js
@@ -1,9 +1,14 @@
 let gridSize = 16;
 
 const gridContainer = document.querySelector("#gridContainer");
-
+//set row equal to an empty div. then, alter makegrid/row function to append the squares to the row
+const row = document.createElement("div");
+row.classList.add("row");
+row.style.height = `calc(var(--gridLength) / ${gridSize})`;
+gridContainer.appendChild(row);
 
 function makeGrid() {
+    
     for(i = 0; i < gridSize; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
@@ -12,8 +17,9 @@ function makeGrid() {
         square.style.borderStyle = "solid";
         square.style.borderColor = "black";
         square.style.borderWidth = "2px";
-        gridContainer.appendChild(square);
+        row.appendChild(square);
     }
+    
 }
 
 makeGrid(); 
