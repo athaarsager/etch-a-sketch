ba4bf6488279f7f630ff8024fb3749b2557dedc4
Andrew Haarsager <athaarsager@gmail.com>
2023-11-11 11:23:10 -0600

    Fix makeGrid function.
    I had to create the elements in the function itself, not outside of it.
    Also added formatting for even borders and removed square height so that
    they do not exceed row height.

--- javascript.js
+++ javascript.js
@@ -1,25 +1,30 @@
 let gridSize = 16;
-
+//rewrite height/width calculations to account for border size
 const gridContainer = document.querySelector("#gridContainer");
-//set row equal to an empty div. then, alter makegrid/row function to append the squares to the row
-const row = document.createElement("div");
-row.classList.add("row");
-row.style.height = `calc(var(--gridLength) / ${gridSize})`;
-gridContainer.appendChild(row);
 
-function makeGrid() {
+function makeRow() {
+
+    const row = document.createElement("div");
+    row.classList.add("row");
+    row.style.height = `calc(var(--gridLength) / ${gridSize})`;
     
     for(i = 0; i < gridSize; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
-        square.style.height = `calc(var(--gridLength) / ${gridSize})`;
         square.style.width = `calc(var(--gridLength) / ${gridSize})`; //references CSS variable, then concatenate js variable
-        square.style.borderStyle = "solid";
-        square.style.borderColor = "black";
-        square.style.borderWidth = "2px";
-        row.appendChild(square);
+        square.style.borderRight = "2px solid black";
+        row.appendChild(square);//all appended to same row
+        console.log("square appended");
     }
-    
+    gridContainer.appendChild(row);
+}
+
+function makeGrid() {
+    for(x=0; x < gridSize; x++) {//change i to x since makeRow already uses and increments i
+        makeRow();
+        console.log("row appended!");
+    }
 }
 
 makeGrid(); 
+
