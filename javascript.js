8e7364b9ede8713477e23c02e4d0ee10aee8c066
Andrew Haarsager <athaarsager@gmail.com>
2023-11-14 16:10:58 -0600

    Fix setGrid to work as intended

--- javascript.js
+++ javascript.js
@@ -1,27 +1,31 @@
 let gridSize = 16;
-//rewrite height/width calculations to account for border size
-const gridContainer = document.querySelector("#gridContainer");
+let newGridSize;
+
+
+let gridContainer = document.querySelector("#gridContainer");
+const container = document.querySelector("#container");
 
 function makeRow() {
-
+    
     const row = document.createElement("div");
     row.classList.add("row");
     row.style.height = `calc(var(--gridLength) / ${gridSize})`;
-    
+
     for(i = 0; i < gridSize; i++) {
         const square = document.createElement("div");
         square.classList.add("square");
         square.style.width = `calc(var(--gridLength) / ${gridSize})`; //references CSS variable, then concatenate js variable
-        square.style.borderRight = "2px solid black";
-        row.appendChild(square);//all appended to same row
+        square.style.borderRight = "1px solid black";
 
         square.addEventListener("mouseover", () => {
             square.style.backgroundColor = "black";
-            console.log("You hovered over a square!");
         });
+        row.appendChild(square);
     }
+    
     gridContainer.appendChild(row);
 }
+
 
 function makeGrid() {
     for(x=0; x < gridSize; x++) {//change i to x since makeRow already uses and increments i
@@ -29,4 +33,26 @@ function makeGrid() {
     }
 }
 
+function removeGrid() {
+    for(i = 0; i < gridSize; i++) {
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
+}
+
+function setGrid() {
+    newGridSize = prompt("Please enter your preferred grid size!");
+    if(newGridSize < 1) {
+        prompt("Invalid number. Grid size must be at least 1.")
+    } else if(newGridSize > 100) {
+        prompt("Invalid number. Grid size must be 100 or less.")
+    }
+    removeGrid();
+    gridSize = newGridSize;
+    makeGrid();
+}
+
 makeGrid(); 
+
+const gridButton = document.querySelector("#gridButton");
+
+gridButton.addEventListener("click", setGrid);
